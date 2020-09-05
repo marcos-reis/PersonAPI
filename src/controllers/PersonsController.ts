ec7ab2509bb28fa8e0145e1c1c7e6e43dd7a86bb
marcos-reis <marcosreisdossantos01@gmail.com>
2020-09-05 14:13:26 +0000

    Add filters for resource

--- src/controllers/PersonsController.ts
+++ src/controllers/PersonsController.ts
@@ -1,42 +1,57 @@
 import { Request, Response } from "express";
 import { PersonsData } from "../data/PersonsData";
-import {calculateAge,generateEmail,defineGenre} from "../utils"
+import { calculateAge, generateEmail, defineGenre } from "../utils"
 import bindDescription from "../utils/bindDescription";
 
-interface IPersonDTO{
-  name:string
-  birthDate:string
-  genre:string
-  description?:string
-  age:number
-  email:string
-  city:string
-  state:string
-  academy:string
-  interest:string
+interface IPersonDTO {
+  name: string
+  birthDate: string
+  genre: string
+  description: string
+  age: number
+  email: string
+  city: string
+  state: string
+  academy: string
+  interest: string
 }
 
-export default class PersonsController{
-  async index(request:Request,response:Response){
+export default class PersonsController {
+  async index(request: Request, response: Response) {
 
-    const persons:IPersonDTO[] = PersonsData.map(person => {
+    const { genre, state, academy } = request.query
+
+    const persons: IPersonDTO[] = PersonsData.map(person => {
 
       return {
-        name:person.name,
-        age:calculateAge(person),
-        email:generateEmail(person),
-        birthDate:person.birthDate,
-        genre:defineGenre(person),
-        description:bindDescription(person),
-        city:person.city,
-        state:person.state,
-        academy:person.academy,
-        interest:person.interest
+        name: person.name,
+        age: calculateAge(person),
+        email: generateEmail(person),
+        birthDate: person.birthDate,
+        genre: defineGenre(person),
+        description: bindDescription(person),
+        city: person.city,
+        state: person.state,
+        academy: person.academy,
+        interest: person.interest
 
       }
     })
 
-    return response.json({persons})
-
+    if (genre) {
+      const personsGenre = persons.filter(person => person.genre === genre)
+      return response.json(personsGenre)
     }
+    if (state) {
+      const personsState = persons.filter(person => person.state === state)
+      return response.json(personsState)
+    }
+    if (academy) {
+      const personsAcademy = persons.filter(person => person.academy === academy)
+      return response.json(personsAcademy)
+    }
+
+    return response.json(persons)
+
+  }
 }
